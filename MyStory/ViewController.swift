552e92dfb74d9f83774f4e8f4fec106a133cdeec
Vahit Emre <vemretellier@gmail.com>
2022-01-07 10:44:26 +0300

    Initial Commit

---
+++ MyStory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyStory
+//
+//  Created by Vahit Emre TELLİER on 7.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
